af2aaedac55135a3e3d63b187bd1d8a6366d4b9a
GabryelVelli <gabryel.velli@gmail.com>
2026-02-27 13:15:47 -0300

    Correção Quando Clicava na tela de Produtos ia pro fim da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Products from "./pages/Products";
 import Quote from "./pages/Quote";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/produtos" element={<Products />} />
